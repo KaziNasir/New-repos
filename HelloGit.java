db2a042c3700e9227b9efef87f346b3c537c06eb
sk-dark <shoumikkazi@gmail.com>
2020-08-23 16:19:51 +0600

    HelloGit.java

---
+++ HelloGit.java
@@ -0,0 +1,13 @@
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++) {
+				
+			System.out.println("Hello World");
+		    System.out.println("Hello Git");
+		}
+
+	}
+
+}
